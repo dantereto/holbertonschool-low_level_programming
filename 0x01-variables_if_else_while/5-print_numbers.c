8ab1a953bc2449de8f01341794a4d09a5392c1a3
dantereto <2247@holbertonschool.com>
2021-02-04 19:19:08 +0000

    print numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - main
+ *Return: return 0
+ */
+int main(void)
+{
+char num = '0';
+while (num <= '9')
+{
+putchar(num);
+num++;
+}
+putchar('\n');
+return (0);
+}
