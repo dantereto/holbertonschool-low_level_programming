f88d00b49d28e23c6e3bd3e94d746ae9a2826d13
dantereto <2247@holbertonschool.com>
2021-02-04 20:19:19 +0000

    prints the numbers from 00 to 99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main
+ *Return: return 0
+ */
+int main(void)
+{
+int num = 0;
+while (num < 100)
+{
+putchar((num / 10) + '0');
+putchar((num % 10) + '0');
+if (num <  99)
+{
+putchar(',');
+putchar(' ');
+}
+num++;
+}
+putchar('\n');
+return (0);
+}
