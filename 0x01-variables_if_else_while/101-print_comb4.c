954558f72e183c031bebbb07dc14f8ae11d9cb50
dantereto <2247@holbertonschool.com>
2021-02-05 23:15:37 +0000

    comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,32 +5,31 @@
  */
 int main(void)
 {
-int num = '0';
-int num3 = '2';
-int num2 = '1';
-while (num <= '7')
-{
-while (num2 <= '8')
-{
-while (num3 <= '9')
-{
-putchar(num);
-putchar(num2);
-putchar(num3);
-if (num != '7' || num2 != '8' || num3 != '9')
-{
-putchar(',');
-putchar (' ');
-}
-num3++;
-}
-num2++;
-num3 = num2 + 1;
-}
-num++;
-num2 = num + 1;
-num3 = num2 + 1;
-}
-putchar('\n');
-return (0);
+  int num = 0;
+  int num2 = 0;
+  while (num < 100)
+    {
+      (num  = num / 10);
+      (num = num % 10);
+      while (num2 < 100)
+	{
+	  (num2 = num2 / 10);
+	  (num2 = num2 % 10);
+	  if (num <  num2 || (num == num2))
+	    {
+	      putchar(num + '0');
+	      putchar(' ' + '0');
+	      putchar(num2 + '0');
+	      if (num != 9 && num2 != 8)
+		{
+		  putchar(',');
+		  putchar(' ');
+		}
+	    }
+	  num2++;
+	}
+      num++;
+    }
+  putchar('\n');
+  return (0);
 }
