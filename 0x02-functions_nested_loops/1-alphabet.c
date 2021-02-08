95a60748e42db8ac95fd5eeb67dff39b5239bd2d
dantereto <2247@holbertonschool.com>
2021-02-08 16:00:31 +0000

    print alphabet with putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,11 +5,11 @@
  */
 void print_alphabet(void)
 {
-  char cont = 'a';
-  while (cont <= 'z')
-    {
-      _putchar(cont);
-      cont++;
-    }
-  _putchar('\n');
+char cont = 'a';
+while (cont <= 'z')
+{
+_putchar(cont);
+cont++;
+}
+_putchar('\n');
 }
