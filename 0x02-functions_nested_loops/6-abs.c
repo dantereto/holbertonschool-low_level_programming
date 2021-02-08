ecafbbeed3509a4e36499614754653380511505b
dantereto <2247@holbertonschool.com>
2021-02-08 20:11:11 +0000

    computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *  _abs -  computes the absolute value of an integer
+ *@n: the number
+ *Return: return 0
+ */
+int _abs(int n)
+{
+if (n < 0)
+return (-n);
+else
+return (n);
+}
