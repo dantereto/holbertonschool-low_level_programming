6d1b33270ca38e3d587ea98d528a485bda8bbd1d
dantereto <2247@holbertonschool.com>
2021-02-08 23:19:02 +0000

    adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ *  add - make a sum of two characters
+ *@a: the first number
+ *@b: the second number
+ *Return: return 0
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
