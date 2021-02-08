07021db34e7f088100ad7b90916bdefc8ade3e6c
dantereto <2247@holbertonschool.com>
2021-02-08 16:31:20 +0000

    checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *  _islower - checks for lowercase character
+ *Return: return 0
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
