c75816b2438a8a2553f453d1c0b58316c8070284
dantereto <2247@holbertonschool.com>
2021-02-08 16:42:13 +0000

    checks for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * _isalpha -  checks for alphabetic character
+ *Return: return 0
+ */
+int _isalpha(int c)
+{
+if (c > 65 && c < 123)
+return (1);
+else
+return (0);
+}
