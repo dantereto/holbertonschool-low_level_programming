2604e56493fe295ecd985a55af6ca16690414e1b
dantereto <2247@holbertonschool.com>
2021-02-09 23:38:32 +0000

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-  long int f1 = 1, f2 = 2, i;
-  long int next;
-  printf("%ld, ", f1);
-  for (i = 2; i < 50; i++)
-    {
-      printf("%ld, ", f2);
-      next = f1 + f2;
-      f1 = f2;
-      f2 = next;
-    }
-  printf("%ld\n", f2);
-  return (0);
+long int f1 = 1, f2 = 2, i;
+long int next;
+printf("%ld, ", f1);
+for (i = 2; i < 50; i++)
+{
+printf("%ld, ", f2);
+next = f1 + f2;
+f1 = f2;
+f2 = next;
+}
+printf("%ld\n", f2);
+return (0);
 }
