fd3e8843472fa1c9f6a5edb1b7adbe490ec50c95
dantereto <2247@holbertonschool.com>
2021-02-09 02:30:43 +0000

    print 50 fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *  main - main function
+ *Return: return 0
+ */
+int main(void)
+{
+int f1 = 1, f2 = 1, i;
+int next;
+printf("%d,", f1);
+for (i = 1; i <= 50; i++)
+{
+printf("%d,", f2);
+next = f1 + f2;
+f1 = f2;
+f2 = next;
+}
+printf("\n");
+return (0);
+}
