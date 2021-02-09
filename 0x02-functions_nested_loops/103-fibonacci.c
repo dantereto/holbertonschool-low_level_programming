a350817fd312329ff82c75376457354f9e7f7e5f
dantereto <2247@holbertonschool.com>
2021-02-09 03:44:09 +0000

    fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *  main - main function
+ *Return: return 0
+ */
+int main(void)
+{
+int two = 2;
+int sum = 0;
+int n = 1;
+while (two <  4000000)
+{
+if (two % 2 == 0)
+sum += two;
+two += n;
+two++;
+}
+printf("%d\n", sum);
+return (0);
+}
