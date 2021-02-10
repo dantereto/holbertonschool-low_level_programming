894aab129e988ba9be83df2462dbeb14b3379a5e
dantereto <2247@holbertonschool.com>
2021-02-10 03:23:19 +0000

    fobonacci4

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,4 +1,6 @@
 #include "holberton.h"
+#include <stdio.h>
+
 /**
  *  main - main
  *Return: return 0
@@ -7,14 +9,14 @@ int main(void)
 {
 int f1 = 1, f2 = 2, i;
 int next;
-printf("%ld, ", f1);
+printf("%d, ", f1);
 for (i = 2; i < 98; i++)
 {
-printf("%ld, ", f2);
+printf("%d, ", f2);
 next = f1 + f2;
 f1 = f2;
 f2 = next;
 }
-printf("%ld\n", f2);
+printf("%d\n", f2);
 return (0);
 }
