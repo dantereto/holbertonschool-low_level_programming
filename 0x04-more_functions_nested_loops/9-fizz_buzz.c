65421b21ff5170e4266c485f4c51d695136c64cd
dantereto <2247@holbertonschool.com>
2021-02-11 22:37:03 +0000

    print a list of numbers and fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - print numbers
+ *Return: return 0
+ */
+int main(void)
+{
+int a = 1;
+while (a <= 100)
+{
+if (a % 3 == 0 && a % 5 == 0)
+printf("FizzBuzz");
+else if (a % 3 == 0)
+printf("Fizz");
+else if (a % 5 == 0)
+printf("Buzz");
+else
+printf("%d", a);
+if (a != 100)
+printf(" ");
+a++;
+}
+printf("\n");
+return (0);
+}
