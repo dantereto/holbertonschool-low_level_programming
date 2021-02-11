844fd0cb0950869bcc17c5ead3ce613f925eab1a
dantereto <2247@holbertonschool.com>
2021-02-11 17:21:37 +0000

    print a list of numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * more_numbers - print a list of numbers
+ *Return: return 0
+ */
+void more_numbers(void)
+{
+int a = 0;
+int b;
+while (a <= 14)
+{
+b = 0;
+while (b <= 14)
+{
+if (b > 9)
+_putchar(b / 10 + '0');
+_putchar(b % 10 + '0');
+b++;
+}
+a++;
+_putchar('\n');
+}
+}
