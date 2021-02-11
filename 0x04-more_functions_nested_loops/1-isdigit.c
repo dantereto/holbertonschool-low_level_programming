27cff854df592a8ee5ec2c88be3e2fe7d7d34b26
dantereto <2247@holbertonschool.com>
2021-02-11 15:39:45 +0000

    isdigit function

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * _isupper -  checks for uppercase character
+ *@c: the number
+ *Return: return 0
+ */
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+return (1);
+else
+return (0);
+}
