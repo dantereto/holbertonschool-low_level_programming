0a2f544f20e4999ed7c6564ff5a4bdc3cd9d6555
dantereto <2247@holbertonschool.com>
2021-02-11 15:57:45 +0000

    multiplies two integer.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * mul -  multiplies two integers
+ *@a:first numer
+ *@b:seconf number
+ *Return: return 0
+ */
+int mul(int a, int b)
+{
+int result = a * b;
+return (result);
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,4 +1,6 @@
 #ifndef HOLBERTON
 #define HOLBERTON
 int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
 #endif
