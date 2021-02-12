6594be48bc7f181cd3b7ccd38793bad451ac150c
dantereto <2247@holbertonschool.com>
2021-02-12 01:43:20 +0000

    print the prime factor of a number

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,17 +7,14 @@ int main(void)
 {
 long int n = 612852475143;
 long int cont, result;
-for (cont = 2; cont < n; cont++)
+for (cont = 2; cont <= n; cont++)
 {
-  if (n % cont == 0)
-    {
-      result = cont;
 while (n % cont == 0)
 {
+result = cont;
 n /= cont;
 }
-    }
- }
+}
 printf("%ld\n", result);
 return (0);
 }
