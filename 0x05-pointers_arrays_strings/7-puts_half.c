867cf8da1e6b3dd79a9ac1af83a07694751f268a
dantereto <2247@holbertonschool.com>
2021-02-15 23:04:13 +0000

    prints half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * puts_half - prints half of a string
+ *@str: the string
+ *Return: return 0
+ */
+void puts_half(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+;
+for (i /= 2; str[i] != '\0'; i++)
+_putchar(str[i]);
+_putchar('\n');
+}
