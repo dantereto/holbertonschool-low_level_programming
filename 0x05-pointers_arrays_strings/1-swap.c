96529d7e12ed8ef0cb048de55f6b70dc041d6385
dantereto <2247@holbertonschool.com>
2021-02-15 18:59:02 +0000

    swap the value of two ints

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * swap_int - updates the value of n
+ *@a: the first int
+ *@b: the seconf int
+ *Return: return 0
+ */
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}
