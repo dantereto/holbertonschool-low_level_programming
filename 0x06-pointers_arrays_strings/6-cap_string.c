5495a1d86c0d614c3ce9acb8fe6d78f4a8823da2
dantereto <2247@holbertonschool.com>
2021-02-18 21:46:49 +0000

    capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,25 +1,28 @@
 #include "holberton.h"
 /**
- * rev_string - reverses a string.
- *@s: the string
+ * cap_string - cap_string
+ *@string: the string
  *Return: return 0
  */
 char *cap_string(char *string)
 {
-  int i  = 0;
-  while (string[i] != 0)
-    {
-      if (string[i] >= 97 && string[i] <= 122)
-	{
-	  if (i == 0)
-	  string[i] -= 32;
-      else if (string[i - 1] == ',' || string[i - 1] == ';' || string[i - 1] == '.' ||
-	       string[i - 1] == 33 || string[i - 1] == '"' || string[i - 1] == ')' || string[i - 1] == '(' 
-	       || string[i - 1] == '{' || string[i - 1] == '}'|| string[i - 1] == '?' || string[i - 1] == '\n' 
-	       || string[i - 1] == 32 || string[i - 1] == '\t')
-	string[i] -= 32;
-	}
-      i++;
-    }
-  return (string);
+int i  = 0;
+while (string[i] != '\0')
+{
+if (string[i] >= 97 && string[i] <= 122)
+{
+if (i == 0)
+string[i] -= 32;
+else if (string[i - 1] == ',' || string[i - 1] == ';'
+|| string[i - 1] == '.' ||
+string[i - 1] == 33 || string[i - 1] == '"' || string[i - 1] ==
+')' || string[i - 1] == '('
+|| string[i - 1] == '{' || string[i - 1] == '}'
+|| string[i - 1] == '?' || string[i - 1] == '\n'
+|| string[i - 1] == 32 || string[i - 1] == '\t')
+string[i] -= 32;
+}
+i++;
+}
+return (string);
 }
