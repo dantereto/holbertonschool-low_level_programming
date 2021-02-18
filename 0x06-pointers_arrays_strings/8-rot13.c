2159ac3029161ee83cb2c1ebf74ddf3c428eccfd
dantereto <2247@holbertonschool.com>
2021-02-18 23:41:19 +0000

    changes the letters to rot13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -6,7 +6,7 @@
  */
 char *rot13(char *string)
 {
-int i = 0, cop;
+int i, cop;
 char rot[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 char num[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 for (i = 0; string[i]; i++)
@@ -14,7 +14,10 @@ for (i = 0; string[i]; i++)
 for (cop = 0; rot[cop]; cop++)
 {
 if (string[i] == rot[cop])
+{
 string[i] = num[cop];
+break;
+}
 }
 }
 return (string);
