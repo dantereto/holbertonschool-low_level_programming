c63147c5e04afc74c711628de39354ae8bd9dc05
dantereto <2247@holbertonschool.com>
2021-02-18 23:27:04 +0000

    changes the letters to rot13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * rot13 - encodes a string into rot13
+ *@string: the string
+ *Return: return 0
+ */
+char *rot13(char *string)
+{
+int i = 0, cop;
+char rot[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char num[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+for (i = 0; string[i]; i++)
+{
+for (cop = 0; rot[cop]; cop++)
+{
+if (string[i] == rot[cop])
+string[i] = num[cop];
+}
+}
+return (string);
+}
