a3d487c2ce8128e05a70707804994b2f8fd5e507
dantereto <2247@holbertonschool.com>
2021-02-19 01:02:34 +0000

    print numbers with putchar

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_number - print int numbers
+ *@n: the number
+ *Return: return 0
+ */
+void print_number(int n)
+{
+unsigned int num = n;
+if (n < 0)
+{
+_putchar('-');
+num = num * -1;
+}
+if ((num / 10) > 0)
+print_number(num / 10);
+_putchar((num % 10) + '0');
+}
