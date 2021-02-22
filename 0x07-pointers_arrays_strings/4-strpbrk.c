c65481cda3cba89257a4d18a304f8a535c11c23d
dantereto <2247@holbertonschool.com>
2021-02-22 21:31:25 +0000

    searches a string for any of a set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _strchr -  locates a character in a string
+ *@s: the pointer
+ *@c : the string
+ *Return: return 0
+ */
+char *_strpbrk(char *s, char *accept)
+{
+  int i, j;
+  for (i = 0; s[i] != '\0'; i++)
+    {
+      for (j = 0; accept[j] != '\0'; j++)
+	{
+	if (s[i] == accept[j])
+	  break;
+	}
+  if (accept[j] != '\0')
+  return (s + i);
+    }
+  return (s);
+}
