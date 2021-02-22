997fc3ced67d369fc6c5f1119cfc2dcf2ec8964f
dantereto <2247@holbertonschool.com>
2021-02-22 17:01:36 +0000

    locates a character in a string!

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _strchr -  locates a character in a string
+ *@s: the pointer
+ *@c : the string
+ *Return: return 0
+ */
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+return (s);
+else if (!*s)
+return ('\0');
+s++;
+}
+return (s);
+}
