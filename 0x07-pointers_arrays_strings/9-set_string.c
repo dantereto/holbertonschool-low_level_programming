f2e3beaef20f3d196756e650d8100d6501bd8ca1
dantereto <2247@holbertonschool.com>
2021-02-23 02:09:05 +0000

    set string

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * set_string - print a chessboard
+ *@s: the double pointer
+ *@to:the pointer
+ *Return: return 0
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
