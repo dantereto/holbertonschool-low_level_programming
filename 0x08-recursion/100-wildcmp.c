9cb89c5c7a067c1f55cd5d535026846daec4a279
dantereto <2247@holbertonschool.com>
2021-02-25 03:49:12 +0000

    wildcmp

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * wildcmp - search if s1 and s1 are similars
+ *@s1: the pointer
+ *@s2:the second pointer
+ *Return: return 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+if (*s1 == '\0' && *s2 == '\0')
+return (1);
+if (*s1 == *s2)
+return (wildcmp(s1 + 1, s2 + 1));
+if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+return (0);
+else
+return (1);
+return (0);
+}
