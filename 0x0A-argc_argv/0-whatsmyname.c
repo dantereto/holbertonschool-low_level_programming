d6c37ed0411e3131b35c33c0f08088d5cccc5463
dantereto <2247@holbertonschool.com>
2021-02-26 15:19:20 +0000

    whatismyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - return the name
+ *@argc: argc
+ *@argv: argv
+ *Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
