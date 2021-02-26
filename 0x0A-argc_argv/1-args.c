03594209521de4e6abaf5de90723dcadad4463cc
dantereto <2247@holbertonschool.com>
2021-02-26 15:28:12 +0000

    print the number of characters

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - return the number of characters
+ *@argc: argc
+ *@argv: argv
+ *Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc);
+return (0);
+}
