fafd960e5f966fd2f5a93f382063a97acdc5245d
dantereto <2247@holbertonschool.com>
2021-03-02 15:41:09 +0000

    create an array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * create_array - create an array
+ *@c: the characters
+ *@size: the size of characters
+ *Return: reyturn 0
+ */
+char *create_array(unsigned int size, char c)
+{
+char *p;
+unsigned int i;
+if (size == 0)
+return ('\0');
+p = malloc(sizeof(char) * size);
+for (i = 0; i < size; i++)
+{
+p[i] = c;
+}
+return (p);
+}
---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,4 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+char *create_array(unsigned int size, char c);
+#endif
