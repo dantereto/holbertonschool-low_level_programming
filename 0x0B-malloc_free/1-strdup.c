590ba910e151f39e6f104c13ed5147fa5c713d98
dantereto <2247@holbertonschool.com>
2021-03-02 16:33:24 +0000

    copy an string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _strdup - copy an strin
+ *@str: the string
+ *Return: reyturn 0
+ */
+char *_strdup(char *str)
+{
+int cont, i;
+char *p;
+if (str == NULL)
+return ('\0');
+for (cont = 0; str[cont] != '\0'; cont++)
+;
+p = malloc(sizeof(char) * cont);
+for (i = 0; i < cont; i++)
+{
+p[i] = str[i];
+}
+p[i] = '\0';
+return (p);
+}
