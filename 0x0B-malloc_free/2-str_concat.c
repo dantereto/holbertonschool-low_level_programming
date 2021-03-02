a245fd164237e1e4a669e6efc5799c377af23879
dantereto <2247@holbertonschool.com>
2021-03-02 20:36:28 +0000

    concat two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,9 +12,9 @@ char *str_concat(char *s1, char *s2)
 int cont = 0, i, copy, j;
 char *p;
 if (s1 == '\0')
-s1 = ("");
+s1 = "";
 if (s2 == '\0')
-s2 = ("");
+s2 = "";
 for (i = 0; s1[i] || s2[i]; i++)
 cont++;
 for (j = 0; s2[j]; j++)
