32603394e3a8e0e09da1a871b864712abc39486d
dantereto <2247@holbertonschool.com>
2021-03-04 19:39:36 +0000

    concat a string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,7 +19,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
   if (array == '\0')
     return ('\0');
   for (i = 0; s1[i] != '\0'; i++, copy++)
-    array[copy] = s1[i];
+    array[i] = s1[i];
   for (j = 0; s2[j] != '\0' && j < n; j++, copy++, i++)
     array[i] = s2[j];
   array[i] = '\0';
