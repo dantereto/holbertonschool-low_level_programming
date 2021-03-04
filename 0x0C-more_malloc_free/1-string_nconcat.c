5b945d08b342446b515291de542e6a68e0889c2b
dantereto <2247@holbertonschool.com>
2021-03-04 17:19:32 +0000

    concat two strins

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,23 +10,25 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i = 0, copy = 0, cont = 0;
-unsigned int j = 0;
-char *array;
-if (s1 == '\0')
-s1 = ("");
-if (s2 == '\0')
-s2 = ("");
-for (i = 0; s1[i] != '\0'; i++)
-cont++;
-array = malloc(sizeof(char) * cont + 1);
-if (array == '\0')
-return ('\0');
-cont = 0;
-for (i = 0; s1[i] != '\0'; i++, copy++)
-array[copy] = s1[i];
-for (j = 0; s2[j] != '\0' && j < n; j++, copy++)
-array[copy] = s2[j];
-return (array);
+  unsigned int i, copy, cont;
+  unsigned int j;
+  char *array;
+  if (s1 == '\0')
+    s1 = ("");
+  if (s2 == '\0')
+    s2 = ("");
+  for (i = 0; s1[i] != '\0'; i++)
+    cont++;
+  for (j = 0; s2[j] != '\0'; j++)
+    cont++;
+  if (n >= j)
+    n = j;
+  array = malloc(sizeof(char) * (i + n + 1));
+  if (array == '\0')
+    return ('\0');
+  for (i = 0; s1[i] != '\0'; i++, copy++)
+    array[copy] = s1[i];
+  for (j = 0; s2[j] != '\0' && j < n; j++, copy++)
+    array[copy] = s2[j];
+  return (array);
 }
-
