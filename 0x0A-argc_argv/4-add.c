15c3cb5b7b1380570b9012c6e78d26a28809b250
dantereto <2247@holbertonschool.com>
2021-03-04 01:59:56 +0000

    add two numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,8 +11,6 @@ int main(int argc, char *argv[])
 int i = 0, sum = 0;
 if (argc < 1)
 return (0);
-if (argc > 1)
-{
 for (i = 1 ; i < argc; i++)
 {
 if (atoi(argv[i]) == 0)
@@ -23,8 +21,5 @@ return (1);
 sum += atoi(argv[i]);
 }
 printf("%d\n", sum);
-}
-else 
-printf("0\n");
 return (0);
 }
