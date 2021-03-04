deaf710dd281c9a9cece0c7f578d95f8a73539a7
dantereto <2247@holbertonschool.com>
2021-03-04 20:55:47 +0000

    concat two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,21 +10,27 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+  unsigned int i = 0, copy = 0, cont = 0;
   unsigned int j;
-  int i;
   char *array;
-  if (s1 == NULL)
-    s1 = "";
-  if (s2 == NULL)
-    s2 = "";
+  if (s1 == '\0')
+    s1 = ("");
+  if (s2 == '\0')
+    s2 = ("");
   for (i = 0; s1[i] != '\0'; i++)
-    array = malloc(sizeof(char) * (i + n + 1));
-  if (array == NULL)
-    return (NULL);
+    cont++;
+  for (j = 0; s2[j] != '\0'; j++)
+    cont++;
+  if (n < cont)
+    j = n;
+  copy += i + j;
+  array = malloc(sizeof(char) * copy + 1);
+  if (array == '\0')
+    return ('\0');
   for (i = 0; s1[i] != '\0'; i++)
     array[i] = s1[i];
-  for (j = 0; j < n && s2[j] != '\0'; j++, i++)
+for (j = 0; s2[j] != '\0' && i < (copy + 1); j++, i++)
     array[i] = s2[j];
-  array[i] = '\0';
+array[i] = '\0';
   return (array);
 }
