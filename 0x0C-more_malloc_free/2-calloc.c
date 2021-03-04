c577f738aba0a139e2b16ffd09e211dfe7bfc049
dantereto <2247@holbertonschool.com>
2021-03-04 14:47:18 +0000

    make a calloc function

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _calloc - make a calloc function
+ *@size: the size
+ *@nmemb: the number of sizes
+ *Return: reyturn 0
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *array;
+unsigned int i;
+if (nmemb == 0 || size == 0)
+return ('\0');
+array = malloc(nmemb * size);
+if (array == '\0')
+return ('\0');
+for (i = 0; i < size; i++)
+array[i] = '\0';
+return (array);
+}
