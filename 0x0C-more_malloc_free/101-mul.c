d538d45036a1914d7eb217bf50549f3efe7bd08f
dantereto <2247@holbertonschool.com>
2021-03-05 02:27:02 +0000

    mul two numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,6 +2,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+ * _strdup - copy an strin
+ *@str: the string
+ *Return: reyturn 0
+ */
+int check(char **av)
+{
+  int j, i;
+  for (i = 1; i < 3; i++)
+    {
+      for (j = 0; av[i][j]; j++)
+	{
+	  if (av[i][j] < '0' || av[i][j] > '9')
+	    return (1);
+	}
+    }
+  return (1);
+}
+/**
  * _realloc - make a realloc function
  *@old_size: the old size
  *@new_size: the new size
@@ -17,6 +35,14 @@ int main(int argc, char *argv[])
       return (0);
     }
   else if (argv[3])
+    {
     printf("Error\n");
-  return (-1);
+    exit(98);
+    }
+  if (argc != 3 || check(argv))
+    {
+      printf("Error\n");
+      exit(98);
+    }
+  return (0);
 }
