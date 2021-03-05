bec5252db6481d323ae1863e3990a763100ab467
dantereto <2247@holbertonschool.com>
2021-03-05 00:26:32 +0000

    make a realloc function

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _realloc - make a realloc function
+ *@old_size: the old size
+ *@new_size: the new size
+ *@ptr: the pointer
+ *Return: reyturn 0
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+char *array;
+char *ptr2;
+unsigned int i;
+if (new_size == old_size)
+return (ptr);
+if (new_size == 0 && ptr != '\0')
+{
+free(ptr);
+return ('\0');
+}
+if (ptr == NULL)
+{
+array = malloc(new_size);
+if (array == '\0')
+return ('\0');
+}
+array = malloc(sizeof(char) * new_size);
+if (array == '\0')
+return ('\0');
+ptr2 = ptr;
+for (i = 0; i < new_size; i++)
+array[i] = *ptr2;
+free(ptr);
+return (array);
+}
