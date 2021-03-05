47a9cc3d98d098a711bf04c263258dce709db58b
dantereto <2247@holbertonschool.com>
2021-03-05 01:42:17 +0000

    make a realloc function

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,37 +10,34 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-  char *array;
-  char *ptr2;
-  unsigned int i;
-  if (new_size == old_size)
-    return (ptr);
-  if (new_size == 0 && ptr != '\0')
-    {
-      free(ptr);
-      return ('\0');
-    }
-  if (ptr == NULL)
-    {
-      array = malloc(new_size);
-      if (array == '\0')
-	return ('\0');
-      return(array);
-    }
-  array = malloc(new_size);
-  if (array == '\0')
-    return ('\0');
-  ptr2 = ptr;
-  if (new_size > old_size)
-    {
-      while (i < old_size)
-	{
-	  array[i] = ptr2[i];
-	  i++;
-	}
-    }
-  for (i = 0;i < old_size && i < new_size; i++)
-    array[i] = ptr2[i];
-  free(ptr);
-  return (array);
- }
+char *array;
+char *ptr2;
+unsigned int i;
+if (new_size == old_size)
+return (ptr);
+if (new_size == 0 && ptr != '\0')
+{
+free(ptr);
+return ('\0');
+}
+if (ptr == NULL)
+{
+array = malloc(new_size);
+if (array == '\0')
+return ('\0');
+return (array);
+}
+array = malloc(new_size);
+if (array == '\0')
+return ('\0');
+ptr2 = ptr;
+if (new_size > old_size)
+{
+for (i = 0; i < old_size; i++)
+array[i] = ptr2[i];
+}
+for (i = 0; i < old_size && i < new_size; i++)
+array[i] = ptr2[i];
+free(ptr);
+return (array);
+}
