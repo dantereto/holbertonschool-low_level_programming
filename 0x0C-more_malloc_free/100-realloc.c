e9d9f0dbc5fab6cdd42b878ee1271e422244e967
dantereto <2247@holbertonschool.com>
2021-03-05 01:36:06 +0000

    make a realloc function

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -25,11 +25,20 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
       array = malloc(new_size);
       if (array == '\0')
 	return ('\0');
+      return(array);
     }
   array = malloc(new_size);
   if (array == '\0')
     return ('\0');
   ptr2 = ptr;
+  if (new_size > old_size)
+    {
+      while (i < old_size)
+	{
+	  array[i] = ptr2[i];
+	  i++;
+	}
+    }
   for (i = 0;i < old_size && i < new_size; i++)
     array[i] = ptr2[i];
   free(ptr);
