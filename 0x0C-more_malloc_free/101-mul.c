9cebe145d9087aaf71fe0b312fba7645e61871d3
dantereto <2247@holbertonschool.com>
2021-03-05 03:40:37 +0000

    mul two numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,6 +2,49 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+ * _calloc - make a calloc function
+ *@size: the size
+ *@nmemb: the number of sizes
+ *Return: reyturn 0
+ */
+int hola(int hola)
+{
+  return (hola);
+}
+/**
+ * _calloc - make a calloc function
+ *@size: the size
+ *@nmemb: the number of sizes
+ *Return: reyturn 0
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+  char *array;
+  unsigned int i;
+  if (nmemb == 0 || size == 0)
+    return ('\0');
+  array = malloc(nmemb * size);
+  if (array == '\0')
+    return ('\0');
+  for (i = 0; i < nmemb * size; i++)
+    array[i] = '\0';
+  return (array);
+}
+/**
+ * _strdup - copy an strin
+ *@str: the string
+ *Return: reyturn 0
+ */
+int _strlen(char*s)
+{
+  int len = 0;
+  while (s[len] != '\0')
+    {
+      len++;
+    }
+  return (len);
+}
+/**
  * _strdup - copy an strin
  *@str: the string
  *Return: reyturn 0
@@ -28,12 +71,18 @@ int check(char **av)
  */
 int main(int argc, char *argv[])
 {
+  char *mem;
+  unsigned int mal;
   (void) argc;
   if (argc != 3)
     {
       printf("Error\n");
       exit(98);
     }
+  mal = _strlen(argv[1]) + _strlen(argv[2]) + 1;
+  mem = _calloc(mal, sizeof(char));
+  if (mem == '\0')
+    return ('\0');
   if (argv[1] && argv[2])
     {
       printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
