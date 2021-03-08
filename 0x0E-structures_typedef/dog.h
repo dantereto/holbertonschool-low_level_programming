6bcced652685c6a2065d74be4faa2a22b30f0429
dantereto <2247@holbertonschool.com>
2021-03-08 16:56:04 +0000

    struct of the dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+/**
+ * struct dog - data of the dog
+ *@name: the name
+ *@age: the age
+ *@owner: the owner
+ *Return: reyturn 0
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
