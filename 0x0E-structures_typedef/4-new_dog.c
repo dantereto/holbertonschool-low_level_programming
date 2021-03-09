db2e038528eba7868d00af54471e7b37ba40f498
dantereto <2247@holbertonschool.com>
2021-03-09 02:44:28 +0000

    create a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,29 @@
 #include <stdlib.h>
 #include "dog.h"
+#include <stdio.h>
+/**
+ * _strdup - copy an strin
+ *@str: the string
+ *Return: reyturn 0
+ */
+char *_strdup(char *str)
+{
+int cont, i;
+char *p;
+if (str == NULL)
+return ('\0');
+for (cont = 0; str[cont] != '\0'; cont++)
+;
+p = malloc(sizeof(char) * cont + 1);
+if (p == 0)
+return ('\0');
+for (i = 0; i < cont; i++)
+{
+p[i] = str[i];
+}
+p[i] = '\0';
+return (p);
+}
 /**
  * new_dog - create a new dog
  *@name: the dog name
@@ -9,35 +33,23 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-char *nam, *own;
-int cont, cont1, i;
-struct dog *dogi;
-dogi = malloc(sizeof(struct dog));
+dog_t *dogi;
+dogi = malloc(sizeof(dog_t));
 if (dogi == '\0')
 return ('\0');
-for (cont = 0; name[cont] != '\0'; cont++)
-;
-for (cont1 = 0; owner[cont1] != '\0'; cont1++)
-;
-nam = malloc(sizeof(char) * cont + 1);
-if (nam == '\0')
+dogi->name = _strdup(name);
+if (dogi->name == '\0')
 {
 free(dogi);
 return ('\0');
 }
-own = malloc(sizeof(char) * cont1 + 1);
-if (own == '\0')
+dogi->owner = _strdup(owner);
+if (dogi->owner == '\0')
 {
-free(name);
+free(dogi->name);
 free(dogi);
 return ('\0');
 }
-for (i = 0; i <= cont; i++)
-nam[i] = name[i];
-for (i = 0; i <= cont1; i++)
-own[i] = owner[i];
-dogi->name = nam;
 dogi->age = age;
-dogi->owner = own;
 return (dogi);
 }
