2da236db5a0b8df36b920406e57ac7d99d578801
dantereto <2247@holbertonschool.com>
2021-03-10 14:31:29 +0000

    print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * print_name - print a name
+ *@name: the name to print
+ *@f: the f pointer
+ *Return: reyturn 0
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name != '\0')
+f(name);
+}
