883153fa8d76582c68702c22708605e9c874375f
dantereto <2247@holbertonschool.com>
2021-03-11 00:41:53 +0000

    check the opcode of main

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,33 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ *@argv: argv
+ *@argc: argc
+ *Return: reyturn 0
+ */
+int main(int argc, char *argv[])
+{
+int number = 0, i = 0;
+char *cont;
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+if (atoi(argv[1]) < 0)
+{
+printf("Error\n");
+exit(2);
+}
+cont  = (char *)main;
+number = atoi(argv[1]);
+while (i < number - 1)
+{
+printf("%02hhx ", cont[i]);
+i++;
+}
+printf("\n");
+return (0);
+}
