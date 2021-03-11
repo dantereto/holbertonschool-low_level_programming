4e93a2e0604184a390f71d52fd09ee0d54dd959b
dantereto <2247@holbertonschool.com>
2021-03-11 17:05:55 +0000

    variadic_functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,5 @@
 #ifndef V_FUNCTIONS_H_
 #define V_FUNCTIONS_H_
-typedef sizeof print_all
-{
-  char *copm;
-  void(*f)();
-}all;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
