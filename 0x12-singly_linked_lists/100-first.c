e3cf0a01045e039eee6c3f45cfeea96fd322194b
dantereto <2247@holbertonschool.com>
2021-03-18 23:33:51 +0000

    print before the main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * add_node - add news nodes to the list
+ * @head: head of the list
+ * @str: the string
+ * Return: NULL.
+ */
+void myStartupFun(void) __attribute__ ((constructor));
+void myStartupFun(void)
+{
+printf("You're beat! and yet, you must allow,
+\nI bore my house upon my back!\n");
+}
