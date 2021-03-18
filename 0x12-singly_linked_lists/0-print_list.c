3064a343f7a673de7c138b93b7d020a91a63c3e6
dantereto <2247@holbertonschool.com>
2021-03-18 16:51:37 +0000

    print the list of elements

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_list - print the elements of the list
+ * @h: head of the linked list
+ * Return: NULL.
+ */
+size_t print_list(const list_t *h)
+{
+size_t cont = 0;
+while (h != '\0')
+{
+if (h->str == '\0')
+{
+printf("[0] %p\n", h->str);
+h = h->next;
+cont++;
+}
+else
+{
+printf("[%d] %s\n", h->len, h->str);
+h = h->next;
+cont++;
+}
+}
+return (cont);
+}
