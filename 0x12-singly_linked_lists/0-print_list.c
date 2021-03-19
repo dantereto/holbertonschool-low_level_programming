c298023df937079d559f6254c53860238bb3da31
dantereto <2247@holbertonschool.com>
2021-03-19 14:51:08 +0000

    print the list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,16 +8,20 @@
 size_t print_list(const list_t *h)
 {
 size_t cont = 0;
-if (h == '\0')
-return (0);
 while (h != '\0')
 {
-if (h->str == NULL)
-printf("[0] (nil)\n");
-else
-printf("[%u] %s\n", h->len, h->str);
-cont++;
+if (h->str == '\0')
+{
+printf("[0] %p\n", h->str);
 h = h->next;
+cont++;
+}
+else
+{
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+cont++;
+}
 }
 return (cont);
 }
