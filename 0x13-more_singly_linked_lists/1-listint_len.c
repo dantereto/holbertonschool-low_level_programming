142bea991c5d41bf7c04ac1d071f10a9f253b4bf
dantereto <2247@holbertonschool.com>
2021-03-22 14:53:16 +0000

    return the list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * listint_len - return the elements of the list
+ * @h: head of the linked list
+ * Return: NULL.
+ */
+size_t listint_len(const listint_t *h)
+{
+size_t cont;
+while (h != '\0')
+{
+h = h->next;
+cont++;
+}
+return (cont);
+}
