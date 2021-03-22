8d4426dd8837dbcd6b2203a0490d66d0a98c20dd
dantereto <2247@holbertonschool.com>
2021-03-22 19:54:00 +0000

    sum the nodes of the list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * sum_listint -sum the nodes of the list
+ * @head: head of the list
+ * Return: NULL.
+ */
+int sum_listint(listint_t *head)
+{
+int number;
+if (head == '\0')
+return (0);
+while (head != '\0')
+{
+number = number + head->n;
+head = head->next;
+}
+return (number);
+}
