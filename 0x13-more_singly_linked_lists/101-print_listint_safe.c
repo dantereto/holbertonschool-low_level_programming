d12eef2c05e5585ad4edb5c6b483232e02327751
dantereto <2247@holbertonschool.com>
2021-03-23 16:54:51 +0000

    print the list safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -20,7 +20,7 @@ position = next_p;
 next_p = next_p->next;
 cont++;
 printf("[%p] %d\n", (void *) position, position->n);
-if (position < next_p)
+if (position <= next_p)
 {
 printf("-> [%p] %d\n", (void *) next_p, next_p->n);
 break;
