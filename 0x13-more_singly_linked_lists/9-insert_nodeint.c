03fc3bea8316adb065342831455006c61ca9843d
dantereto <2247@holbertonschool.com>
2021-03-23 13:39:53 +0000

    insert a new node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,11 +13,17 @@ unsigned int number;
 new = malloc(sizeof(listint_t));
 if (new == '\0')
 return ('\0');
+if (idx == 0)
+{
+new->next = *head;
+*head = new;
+return (new);
+}
 new->n = n;
 cont = *head;
 for (number = 0; number < idx - 1; number++)
 {
-if (cont == '\0' || cont->next == '\0')
+if (cont == '\0')
 return ('\0');
 cont = cont->next;
 }
