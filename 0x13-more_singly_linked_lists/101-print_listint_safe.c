c84a056eeab1c92075383bfc39cea95acc19accc
dantereto <2247@holbertonschool.com>
2021-03-23 16:36:39 +0000

    print the list safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,11 +11,9 @@ size_t print_listint_safe(const listint_t *head)
 {
 const listint_t *position, *next_p = head;
 size_t cont = 0;
-if (next_p == 0)
-return (0);
 if (head == '\0')
 return ('\0');
-while (next_p != '\0')
+while (next_p != 0)
 {
 position = next_p;
 next_p = next_p->next;
