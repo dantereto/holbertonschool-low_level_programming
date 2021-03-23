708c7351d712c7713f5513776e8b82ac5087bd99
dantereto <2247@holbertonschool.com>
2021-03-23 17:08:45 +0000

    print the list safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,19 +9,21 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-  const listint_t *position, *next_p = head;
-  size_t cont = 0;
-  if (head == '\0')
-    return ('\0');
-  while (next_p != 0)
-    {
-      position = next_p;
-      next_p = next_p->next;
-      cont++;
-      printf("[%p] %d\n", (void *) position, position->n);
-      if (position <= next_p)
-	{
-	  printf("-> [%p] %d\n", (void *) next_p, next_p->n);
+const listint_t *position, *next_p = head;
+size_t cont = 0;
+if (head == '\0')
+return ('\0');
+if (next_p == 0)
+exit(98);
+while (next_p != 0)
+{
+position = next_p;
+next_p = next_p->next;
+cont++;
+printf("[%p] %d\n", (void *) position, position->n);
+if (position <= next_p)
+{
+printf("-> [%p] %d\n", (void *) next_p, next_p->n);
 break;
 }
 }
