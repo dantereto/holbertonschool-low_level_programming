66d9738a6db3b8cf7d5d85084f4ab86a421f31a1
dantereto <2247@holbertonschool.com>
2021-03-25 21:23:57 +0000

    get s specific byte

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,14 +9,12 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 unsigned int i;
-if (n)
-{
+if (index > sizeof(n) * 2)
+return (-1);
 for (i = 0; i < index; i++)
 n = n >> 1;
 if (n & 1)
 return (1);
 else
 return (0);
-}
-return (-1);
 }
