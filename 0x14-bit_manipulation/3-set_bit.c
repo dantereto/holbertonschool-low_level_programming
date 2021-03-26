cf34e525fc662cc87d5a24ea7c214ac3f086cb48
dantereto <2247@holbertonschool.com>
2021-03-26 00:08:48 +0000

    set a bit on 1 to index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * set_bit - set the bit on 1 to index
+ *@n: the number
+ *@index: the index
+ *Return: reyturn 0
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+if (index > sizeof(n) * 8)
+return (-1);
+else
+{
+*n = (*n | 1 >> index);
+return (1);
+}
+}
