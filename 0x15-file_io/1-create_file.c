78cd7c061010cb82b65e0c5e0e54e23ea7a03df9
dantereto <2247@holbertonschool.com>
2021-03-29 16:55:30 +0000

    create a file with permissions

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * create_file - create a file with r and w permissions
+ *@text_content: the cotent of the file
+ *@filename: the name of the file
+ *Return: reyturn 0
+ */
+int create_file(const char *filename, char *text_content)
+{
+int open_f, cont, write_f;
+if (filename == '\0')
+return (-1);
+open_f = open(filename, O_WRONLY | O_CREAT | O_APPEND | O_TRUNC ,S_IRUSR | S_IWUSR);
+if (open_f == '\0')
+return (-1);
+if (text_content)
+{
+for (cont = 0; text_content[cont] != '\0'; cont++)
+{
+}
+write_f = write(open_f, text_content, cont);
+if (write_f =='\0')
+return (-1);
+}
+close(open_f);
+return (1);
+}
