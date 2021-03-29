e277d845943f3eddcfff3e8a2406f36715b6d777
dantereto <2247@holbertonschool.com>
2021-03-29 18:24:38 +0000

    read the file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,18 +9,18 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 char *buff;
-int open_f;
+int open_f = 0;
 ssize_t read_f, write_f;
 if (filename == '\0')
 return (0);
 open_f = open(filename, O_RDWR);
-if (open_f == '\0')
+if (open_f == -1)
 return (0);
 buff = malloc(sizeof(char) * letters);
 if (buff == '\0')
 return (0);
 read_f = read(open_f, buff, letters);
-if (read_f == '\0')
+if (read_f == -1)
 free(buff);
 return (0);
 write_f = write(STDOUT_FILENO, buff, read_f);
