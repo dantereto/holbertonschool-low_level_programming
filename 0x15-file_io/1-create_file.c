64c3d656a2fdd4beb89da0b54fdf997c6ed8ea40
dantereto <2247@holbertonschool.com>
2021-03-29 18:38:51 +0000

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int open_f, cont, write_f = 0;
+int open_f = 0, cont = 0, write_f = 0;
 if (filename == '\0')
 return (-1);
 open_f = open(filename, O_WRONLY | O_CREAT | O_APPEND | O_TRUNC ,S_IRUSR | S_IWUSR);
