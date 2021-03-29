8c3d489a63fbe76b72095bd77f7e0da8c5ef7ac7
dantereto <2247@holbertonschool.com>
2021-03-29 18:41:59 +0000

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,8 +11,9 @@ int create_file(const char *filename, char *text_content)
 int open_f = 0, cont = 0, write_f = 0;
 if (filename == '\0')
 return (-1);
-open_f = open(filename, O_WRONLY | O_CREAT | O_APPEND | O_TRUNC ,S_IRUSR | S_IWUSR);
-if (open_f == '\0')
+open_f = open(filename, O_WRONLY | O_CREAT |
+O_APPEND | O_TRUNC, S_IRUSR | S_IWUSR);
+if (open_f == -1)
 return (-1);
 if (text_content)
 {
@@ -20,7 +21,7 @@ for (cont = 0; text_content[cont] != '\0'; cont++)
 {
 }
 write_f = write(open_f, text_content, cont);
-if (write_f =='\0')
+if (write_f == -1 || write_f == -1)
 return (-1);
 }
 close(open_f);
