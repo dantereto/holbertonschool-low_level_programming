938d31482029756486ee83b79ebdba8925bb26f1
dantereto <2247@holbertonschool.com>
2021-04-30 16:31:25 +0000

    print elements of the list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,19 +2,18 @@
 #include <string.h>
 #include "lists.h"
 /**
- * add_nodeint - add news nodes to the list
- * @head: head of the list
- * @n: the number
+ * print_dlistint - print elements of the list
+ * @h: head of the list
  * Return: NULL.
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-  size_t cont = 0;
-  while (h != '\0')
-    {
-      printf("%d\n", h->n);
-      h = h->next;
-      cont++;
-    }
-  return (cont);
+size_t cont = 0;
+while (h != '\0')
+{
+printf("%d\n", h->n);
+h = h->next;
+cont++;
+}
+return (cont);
 }
