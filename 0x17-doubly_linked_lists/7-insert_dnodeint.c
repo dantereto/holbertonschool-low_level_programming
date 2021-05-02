ebe890b5e90322d6f42360e8d034b49e2f731ddc
dantereto <2247@holbertonschool.com>
2021-05-02 00:08:00 +0000

    inter

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,17 +12,19 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 dlistint_t *new, *cont = *h;
 unsigned int number = 0;
 new = malloc(sizeof(dlistint_t));
-if (new == '\0')
-return ('\0');
+if (new == NULL)
+  return (NULL);
 new->n = n;
 if (idx == 0)
 {
-add_dnodeint (h, n);
+return (NULL);
+new->n = n;
+if (idx == 0)
 }
 for (number = 0; number < idx - 1; number++)
 {
-if (cont == '\0')
-return ('\0');
+if (cont == NULL)
+return (NULL);
 cont = cont->next;
 }
 new->next = cont->next;
