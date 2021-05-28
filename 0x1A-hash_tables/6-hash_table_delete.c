e27201055fa19f5abe117e2d7c45cbca60393f2c
dantereto <2247@holbertonschool.com>
2021-05-28 14:23:13 +0000

    delete

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,28 +1,31 @@
 #include "hash_tables.h"
 /**
- * sum_listint -sum the nodes of the list
- * @head: head of the list
+ * hash_table_delete - delete a nodem of the list
+ * @ht: head of the list
  * Return: NULL.
  */
 void hash_table_delete(hash_table_t *ht)
 {
-  hash_node_t *hash, *copy;
-  unsigned long int cont = 0;
-  if (!ht)
-    return;
-  while (cont < ht->size)
-    {
-      hash = ht->array[cont];
-      while (hash)
-	{
-	  copy = hash;
-	  hash = hash->next;
-	  free(copy->key);
-	  free(copy->value);
-	  free(copy);
-	}
-      cont++;
-    }
-  free(ht->array);
-  free(ht);
+hash_node_t *hash, *copy;
+unsigned long int cont = 0;
+if (!ht)
+return;
+else
+{
+while (cont < ht->size)
+{
+hash = ht->array[cont];
+while (hash)
+{
+copy = hash;
+hash = hash->next;
+free(copy->key);
+free(copy->value);
+free(copy);
+}
+cont++;
+}
+}
+free(ht->array);
+free(ht);
 }
