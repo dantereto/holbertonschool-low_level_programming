6cb6002bd7b2051a99f0c4612585556ee2a61dd3
dantereto <2247@holbertonschool.com>
2021-05-28 00:57:23 +0000

    betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,30 +10,30 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-  hash_node_t *new, *temp;
-  unsigned long int index;
+hash_node_t *new, *temp;
+unsigned long int index;
 
-  if (ht == NULL || key == NULL || value == NULL)
-    return (0);
+if (ht == NULL || key == NULL || value == NULL)
+return (0);
 
-  index = key_index((const unsigned char *)key, ht->size);
-  temp = ht->array[index];
-      while (temp)
-	{
-	  if (strcmp(temp->key, key) == 0)
-	    {
-	      temp->value = strdup(value);
-	      return (1);
-	    }
-	  temp = temp->next;
-	}
-  new = malloc(sizeof(hash_node_t));
-  if (new == NULL)
-    return (0);
-  new->key = strdup(key);
-  new->value = strdup(value);
-  new->next = NULL;
-    new->next = ht->array[index];
-  ht->array[index] = new;
-  return (1);
+index = key_index((const unsigned char *)key, ht->size);
+temp = ht->array[index];
+while (temp)
+{
+if (strcmp(temp->key, key) == 0)
+{
+temp->value = strdup(value);
+return (1);
+}
+temp = temp->next;
+}
+new = malloc(sizeof(hash_node_t));
+if (new == NULL)
+return (0);
+new->key = strdup(key);
+new->value = strdup(value);
+new->next = NULL;
+new->next = ht->array[index];
+ht->array[index] = new;
+return (1);
 }
--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,22 +1,23 @@
 #include "hash_tables.h"
 /**
- * sum_listint -sum the nodes of the list
- * @head: head of the list
+ * hash_table_get -set the table
+ * @ht: the hash tables
+ * @key: the key
  * Return: NULL.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-  unsigned long int index;
-  hash_node_t *copy;
-  if (ht == '\0' || key == '\0')
-    return('\0');
-  index = key_index((const unsigned char *)key, ht->size);
-  copy = ht->array[index];
-  while (copy)
-    {
-      if (strcmp(copy->key, key) == 0)
-	return (copy->value);
-      copy = copy->next;
-    }
-  return (NULL);
+unsigned long int index;
+hash_node_t *copy;
+if (ht == '\0' || key == '\0')
+return ('\0');
+index = key_index((const unsigned char *)key, ht->size);
+copy = ht->array[index];
+while (copy)
+{
+if (strcmp(copy->key, key) == 0)
+return (copy->value);
+copy = copy->next;
+}
+return (NULL);
 }
