81076079661ea265444655b5fcdec43b5eee84d7
dantereto <2247@holbertonschool.com>
2021-08-30 20:19:08 -0500

    interpolation

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,34 @@
+#include "search_algos.h"
+/**
+ *  interpolation_search - interpolation_search
+ *
+ * @array: array
+ * @size: size of array
+ * @value: the value to search
+ * Return: Always 0 (Success)
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+size_t low = 0, high = size - 1;
+if (array == NULL)
+return (-1);
+while (low <= high)
+{
+size_t pos = low + (((double)(high - low) / (
+			     array[high] - array[low])) * (
+				     value - array[low]));
+if (pos >= size)
+{
+printf("Value checked array[%lu] is out of range\n", pos);
+return (-1);
+}
+printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+if (array[pos] == value)
+return (pos);
+else if (array[pos] < value)
+high = pos - 1;
+else if (array[pos] > value)
+low = pos + 1;
+}
+return (-1);
+}
