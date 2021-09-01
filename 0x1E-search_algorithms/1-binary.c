411fc0d762d621bb19f790b875fbef524546f888
dantereto <2247@holbertonschool.com>
2021-09-01 09:24:36 -0500

    2

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -14,12 +14,12 @@ if (array != NULL)
 {
 for (left = 0, right = size - 1; left <= right;)
 {
-printf("Searching in array:");
+printf("Searching in array: ");
 for (i = left; i < right; i++)
 {
 printf("%i, ", array[i]);
 }
-printf("%i\n", array[i]);
+printf("%i\n", array[right]);
 middle = (left + right) / 2;
 if (array[middle] > value)
 right = middle - 1;
