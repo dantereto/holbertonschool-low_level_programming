d274c6f2bb541cf720a0e13db347fb252306bf2b
dantereto <2247@holbertonschool.com>
2021-09-01 10:41:41 -0500

    check

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -25,9 +25,9 @@ return (-1);
 printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 if (array[pos] == value)
 return (pos);
-else if (array[pos] < value)
-high = pos - 1;
 else if (array[pos] > value)
+high = pos - 1;
+else if (array[pos] < value)
 low = pos + 1;
 }
 return (-1);
